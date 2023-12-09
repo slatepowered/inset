eeb46052da9d134e20260b88ae69194b1d047498
orbyfied <atomf88zero@gmail.com>
2023-12-09 23:23:03 +0100

    c

--- inset-core/src/main/java/slatepowered/inset/query/FindAllStatus.java
+++ inset-core/src/main/java/slatepowered/inset/query/FindAllStatus.java
@@ -1,6 +1,7 @@
 package slatepowered.inset.query;
 
 import slatepowered.inset.codec.DataCodec;
+import slatepowered.inset.datastore.DataItem;
 import slatepowered.inset.datastore.Datastore;
 import slatepowered.inset.datastore.OperationStatus;
 import slatepowered.inset.modifier.Projection;
@@ -8,12 +9,15 @@ import slatepowered.inset.modifier.Sorting;
 import slatepowered.inset.source.DataSourceBulkIterable;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * The status/result of a {@link Datastore#findAll(Query)} operation.
@@ -28,8 +32,48 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      */
     protected DataSourceBulkIterable iterable;
 
+    /**
+     * Whether the source iterable had any items.
+     */
+    protected boolean sourceHadAny;
+
+    /**
+     * The stream of cached items, this will be null if
+     * no cached items are used in this query.
+     */
+    protected Stream<? extends DataItem<K, T>> cachedStream;
+
+    /**
+     * The list of cached items, this will be null if
+     * no cached items are used in this query.
+     */
+    protected List<? extends DataItem<K, T>> cachedItems;
+
+    /**
+     * The stream of items.
+     */
+    protected Stream<? extends FoundItem<K, T>> stream;
+
+    // The cached stream iterator
+    protected Iterator<? extends FoundItem<K, T>> streamIterator;
+
     public FindAllStatus(Datastore<K, T> datastore, Query query) {
         super(datastore, query);
+    }
+
+    // update the current stream instance to the given instance
+    private void updateStream(Stream<? extends FoundItem<K, T>> stream) {
+        this.stream = stream;
+    }
+
+    // ensure the presence of a usable stream iterator to apply
+    // to the current output stream, this is a terminal operation
+    private Iterator<? extends FoundItem<K, T>> streamIterator() {
+        if (streamIterator == null) {
+            this.streamIterator = stream.iterator();
+        }
+
+        return streamIterator;
     }
 
     /**
@@ -101,11 +145,11 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
     }
 
     /**
-     * Get the backing iterable for this status.
+     * Get the backing source iterable for this status.
      *
      * @return The backing iterable.
      */
-    public DataSourceBulkIterable iterable() {
+    public DataSourceBulkIterable sourceIterable() {
         return iterable;
     }
 
@@ -116,7 +160,7 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      * @return This.
      */
     public FindAllStatus<K, T> batch(int size) {
-        iterable = iterable.batch(size);
+        iterable.batch(size);
         return this;
     }
 
@@ -127,7 +171,7 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      * @return This.
      */
     public FindAllStatus<K, T> limit(int size) {
-        iterable = iterable.limit(size);
+        stream = stream.limit(size);
         return this;
     }
 
@@ -138,7 +182,8 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      * @return This.
      */
     public FindAllStatus<K, T> filter(Query query) {
-        iterable = iterable.filter(query);
+        if (cachedStream != null) throw new UnsupportedOperationException("Filtering on a partially cached iterable is currently not supported");
+        else iterable = iterable.filter(query);
         return this;
     }
 
@@ -175,17 +220,35 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      * @return This.
      */
     public FindAllStatus<K, T> sort(Sorting sorting) {
-        iterable.sort(sorting);
+        if (cachedStream != null) {
+            // todo: efficient stream sorting and a (partial) sorting comparator generator
+            throw new UnsupportedOperationException("Sorting on a partially cached iterable is currently unsupported");
+        } else {
+            iterable.sort(sorting);
+        }
+
         return this;
+    }
+
+    /**
+     * Check whether there are/were any items as a result.
+     *
+     * @return If there is any data.
+     */
+    public boolean hasAny() {
+        return cachedItems != null ? !cachedItems.isEmpty() || sourceHadAny : sourceHadAny;
     }
 
     /**
      * Check whether there is another item to be loaded.
      *
+     * This is a terminal operation for the backing stream. meaning after this
+     * you can no longer use other terminal methods like {@link #first()}.
+     *
      * @return Whether there is another item.
      */
     public boolean hasNext() {
-        return iterable.hasNext();
+        return iterable.hasNext() || cachedStream != null ? streamIterator().hasNext() : iterable.hasNext();
     }
 
     // asynchronously execute the given function
@@ -204,10 +267,15 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      * This may have to fetch it from the database which could take time. It is recommended to
      * execute this function asynchronously.
      *
+     * This is a terminal operation for the backing stream. meaning after this
+     * you can no longer use other terminal methods like {@link #first()}.
+     *
      * @return The item.
      */
-    public Optional<FoundItem<K, T>> next() {
-        return iterable.next().map(this::qualify);
+    public Optional<? extends FoundItem<K, T>> next() {
+        return cachedStream != null ?
+                streamIterator().hasNext() ? Optional.of(streamIterator().next()) : Optional.empty() :
+                iterable.next().map(this::qualify);
     }
 
     /**
@@ -215,22 +283,29 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      *
      * This may have to fetch it from the database.
      *
+     * This is a terminal operation for the backing stream. meaning after this
+     * you can no longer use other terminal methods like {@link #first()}.
+     *
      * @return The item.
      */
-    public CompletableFuture<Optional<FoundItem<K, T>>> nextAsync() {
+    public CompletableFuture<Optional<? extends FoundItem<K, T>>> nextAsync() {
         return async(this::next);
     }
 
     /**
-     * Get, fetch and qualify the first item if present.
+     * Get, fetch and qualify the first item if present, this is a terminal operation.
      *
      * This may have to fetch it from the database which could take time. It is recommended to
      * execute this function asynchronously.
      *
+     * This is a terminal operation, meaning after this this iterable is closed.
+     *
      * @return The item.
      */
-    public Optional<FoundItem<K, T>> first() {
-        return iterable.first().map(this::qualify);
+    public Optional<? extends FoundItem<K, T>> first() {
+        return cachedStream != null ?
+                stream.findFirst() :
+                iterable.first().map(this::qualify);
     }
 
     /**
@@ -238,9 +313,11 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      *
      * This may have to fetch it from the database.
      *
+     * This is a terminal operation, meaning after this this iterable is closed.
+     *
      * @return The item.
      */
-    public CompletableFuture<Optional<FoundItem<K, T>>> firstAsync() {
+    public CompletableFuture<Optional<? extends FoundItem<K, T>>> firstAsync() {
         return async(this::first);
     }
 
@@ -250,11 +327,17 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      * This most likely has to fetch the items from the database, so it
      * is recommended to call this asynchronously.
      *
+     * This is a terminal operation, meaning after this this iterable is closed.
+     *
      * @return The list of items.
      */
     @SuppressWarnings("unchecked")
-    public List<FoundItem<K, T>> list() {
-        List<FoundItem<K, T>> list = (List<FoundItem<K,T>>) (Object) iterable.list();
+    public List<? extends FoundItem<K, T>> list() {
+        if (cachedStream != null) {
+            return stream.collect(Collectors.toList());
+        }
+
+        List<? extends FoundItem<K, T>> list = (List<? extends FoundItem<K,T>>) iterable.list();
         for (FoundItem<K, T> item : list) {
             this.qualify(item);
         }
@@ -267,9 +350,11 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      *
      * This most likely has to fetch the items from the database.
      *
+     * This is a terminal operation, meaning after this this iterable is closed.
+     *
      * @return The list of items.
      */
-    public CompletableFuture<List<FoundItem<K, T>>> listAsync() {
+    public CompletableFuture<List<? extends FoundItem<K, T>>> listAsync() {
         return async(this::list);
     }
 
@@ -281,8 +366,8 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
      *
      * @return The stream of items.
      */
-    public Stream<FoundItem<K, T>> stream() {
-        return iterable.stream().map(this::qualify);
+    public Stream<? extends FoundItem<K, T>> stream() {
+        return stream;
     }
 
     public boolean failed() {
@@ -299,11 +384,37 @@ public class FindAllStatus<K, T> extends OperationStatus<K, T, FindAllStatus<K, 
     }
 
     /**
+     * Zip the given list of cached items to the current stream of items.
+     *
+     * @param list The list of cached items.
+     * @return This.
+     */
+    public synchronized FindAllStatus<K, T> withCached(List<? extends DataItem<K, T>> list) {
+        if (this.cachedStream != null)
+            throw new IllegalStateException("Already has an attached cached stream");
+        this.cachedItems = list;
+        this.cachedStream = list.stream();
+        this.stream = this.stream != null ?
+                Stream.concat(this.stream, cachedStream) :
+                cachedStream;
+        return this;
+    }
+
+    /**
      * Complete this query with the given parameters.
      */
     protected synchronized FindAllStatus<K, T> completeInternal(DataSourceBulkIterable iterable, Object error) {
         this.iterable = iterable;
         this.error = error;
+
+        if (iterable != null) {
+            this.sourceHadAny = iterable.hasNext();
+
+            // update stream with iterable items
+            Stream<FoundItem<K, T>> iterableStream = iterable.stream().map(this::qualify);
+            updateStream(stream != null ? Stream.concat(stream, iterableStream) : iterableStream);
+        }
+
         completeInternal(this);
         return this;
     }
